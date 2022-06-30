a1476cf4f1ef8ef708dfb3c3995d8c9d1fde9895
Mcusac <matthew.d.cusack@gmail.com>
2022-06-30 18:49:35 -0500

    thurs1

--- static/js/logic(notes).js
+++ static/js/logic(notes).js
@@ -1,26 +1,19 @@
-function createFeatures(earthquakeData) {
+var legend = L.control({position: 'bottomright'});
+console.log(geojson.options.limits);
+console.log(geojson.options.colors);
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = geojson.options.limits.map( m => Math.round(m)),
+        labels = [];
 
-    function onEachFeature(feature, layer) {
-    
-        layer.biindPopup(`<h3>${feature.properties.place}</h3><hr>`)
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(i) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
-    function changeColor(feature) {
-        if (feature.properties.mag > 2)
-        return 'black'
-        else if (feature.properties.mag > 1)
-        return 'orange'
-        else
-        return 'red'
-    }
-    function geojsonMarkerOptions(feature) {
-        return {
-            radius: feature.geometry.coordinates[2],
-            fillColor: changeColor(feature),
-            color: '#000',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
 
-        }
-    }
-}
+    return div;
+};
+
+legend.addTo(myMap);
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
@@ -17,10 +17,40 @@ function createFeatures(earthquakeData) {
     layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
   }
 
+  // Define function for changing color based oon magnitude of earthquake
+  function changeColor(feature) {
+    if (feature.properties.mag > 2)
+    return 'black'
+    else if (feature.properties.mag > 1)
+    return 'orange'
+    else
+    return 'red'
+  };
+
+  function changeSize(feature) {
+    if (feature.geometry.coordinates[2] > 100)
+    return 0
+    else return
+    feature.geometry.coordinates[2]
+  };
   // Create a GeoJSON layer that contains the features array on the earthquakeData object.
   // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng)
+    },
+    style: function geojsonMarkerOptions(feature) {
+      return {
+          radius: changeSize(feature),
+          fillColor: changeColor(feature),
+          color: '#000',
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+  
+      }
+    }
   });
 
   // Send our earthquakes layer to the createMap function/
@@ -30,27 +60,27 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
   // Create a baseMaps object.
-  var baseMaps = {
+  let baseMaps = {
     "Street Map": street,
     "Topographic Map": topo
   };
 
   // Create an overlay object to hold our overlay.
-  var overlayMaps = {
+  let overlayMaps = {
     Earthquakes: earthquakes
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  var myMap = L.map("map", {
+  let myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
@@ -68,40 +98,25 @@ function createMap(earthquakes) {
 }
 
 // Legend
-// var legend = L.control({position: 'bottomright'});
+let legend = L.control({position: 'bottomright'});
 
-// legend.onAdd = function (map) {
+legend.onAdd = function (map) {
 
-//     var div = L.DomUtil.create('div', 'info legend'),
-//         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-//         labels = [];
+    let div = L.DomUtil.create('div', 'info legend'),
+      // grades = geojson.options.limits.map( m => Math.round(m)),
+      grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+      labels = [];
 
-//     // loop through our density intervals and generate a label with a colored square for each interval
-//     for (var i = 0; i < grades.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-//     }
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + changeColor(i) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
 
-//     return div;
-// };
+    return div;
+};
 
-// legend.addTo(map);
+legend.addTo(mymap);
 
 
-// var legend = L.control({position: 'bottomright'});
-// console.log(geojson.options.limits);
-// console.log(geojson.options.colors);
-// legend.onAdd = function (map) {
-//     var div = L.DomUtil.create('div', 'info legend'),
-//         grades = geojson.options.limits.map( m => Math.round(m)),
-//         labels = [];
-//     // loop through our density intervals and generate a label with a colored square for each interval
-//     for (var i = 0; i < grades.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + getColor(i) + '"></i> ' +
-//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-//     }
-//     return div;
-// };
-// legend.addTo(myMap);
